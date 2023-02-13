a9f6af34fda8df807ee1b1447d16ed19d5b5fde3
Dachev <dachev1919@gmail.com>
2023-02-13 22:05:13 +0200

    Adding rtk query

---
+++ src/store/slices/apiSlice.tsx
@@ -0,0 +1,17 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+import { IProduct } from '../../modules/feed/components/product/Product';
+
+// if you need to scale the application
+export const productsApi = createApi({
+  reducerPath: 'productsApi',
+  baseQuery: fetchBaseQuery({
+    baseUrl: 'https://63e21a47109336b6cbff8c48.mockapi.io/',
+  }),
+  endpoints: (builder) => ({
+    getProductsByPermalink: builder.query<IProduct[], string>({
+      query: (permalink) => `${permalink}`,
+    }),
+  }),
+});
+
+export const { useGetProductsByPermalinkQuery } = productsApi;
--- src/store/store.ts
+++ src/store/store.ts
@@ -1,12 +1,16 @@
 import { configureStore } from '@reduxjs/toolkit';
 import productFilterSlice from './slices/filterSlice';
 import cartSlice from './slices/cartSlice';
+import { productsApi } from './slices/apiSlice';
 
 export const store = configureStore({
   reducer: {
     filter: productFilterSlice,
     cart: cartSlice,
+    [productsApi.reducerPath]: productsApi.reducer,
   },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(productsApi.middleware),
 });
 
 export type RootState = ReturnType<typeof store.getState>;
